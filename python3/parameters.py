50b287178796215b9f9cd754e0804d27635ec8a6
Ivo Zizak <ivo@soak.local>
2021-12-25 17:15:58 +0100

    added comments about parameters

--- python3/parameters.py
+++ python3/parameters.py
@@ -30,15 +30,26 @@ simappname="abs_08"
 
 # V is for Voxel
 Vsize=2E-7               #  m - I'd like 10nm, but no computer is going ot make it
-#to test the code, use larger voxel size:
+
+#to test the code in a fast run, use larger voxel size, but take care that it is not larger than the box
 Vsize=2E-6
 
+# these describe the dimensions of the calculated volume
+# it makes sense that the box size is larger than the beam diameter
 sample_thickness=3.0E-4    # 0.3 mm
 beam_dia = 3.2E-5 # 20 mu
+# this is actually setting the half of the size of the box: the actual box is from -box_Y_size to box_Y_size:
 box_Y_size = 30.0E-6 # 50 mu
 box_Z_size = 17.0E-6 # 35 mu
 
+# Intensity of the primary beam is used to get the dose values in a realistic order of magnitudes.
+# the funny numer here is taken from the experimental data
 I0=20334375 # photon/s - flux, beam intensity
+
+# the list of primary beam energies to perform the calculation for. 
+# one can run multiple energies or make a different parameter file for each
+# E_beam=[5000,8000,12000,18000,25000, 30000,35000]
+E_beam=[8000]
 
 
 #############################################
@@ -77,7 +88,3 @@ fluorescence_yield_K={
 #
 # Energy of the L-lines is 0.3-0.4 keV
 #
-
-E_beam=[5000,8000,12000,18000,25000, 30000,35000]
-E_beam=[5000,12000,25000]
-E_beam=[3700]
