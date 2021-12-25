6682b99e62254e6ccc44430d7e8755e155dac688
Ivo Zizak <ivo@soak.local>
2021-12-25 16:45:21 +0100

    removed stale code

--- python3/absorption.py
+++ python3/absorption.py
@@ -30,20 +30,6 @@ appname="{}/{}".format(datadir,appname)
 simappname1="{}/{}".format(datadir,simappname1)
 simappname2="{}/{}".format(datadir,simappname2)
 
-
-#atomic_constants=np.genfromtxt("atomic_constants.dat")
-elementsList = (
-    'none', 'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne',
-    'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V',
-    'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br',
-    'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag',
-    'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr',
-    'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu',
-    'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi',
-    'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U')
-
-#print atomic_constants[10]
-
         
 class beam():
     def __init__():
